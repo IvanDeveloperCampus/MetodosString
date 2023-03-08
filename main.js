4297f21ff308ef74e52535814cec24bb8ffcbe23
Ivan Sanchez <iivaan437@gmail.com>
2023-03-08 16:07:13 -0500

    feat: :sparkles: Operador Logico OR
    Uso del operador logico OR  ||

--- main.js
+++ main.js
@@ -2,14 +2,13 @@
 
 //OPERADORES LOGICOS AND(&&), OR( ), NOT(!)
 
-///El operador && (AND) devuelve true si ambos operandos son
- //verdaderos y false en cualquier otro caso. Por ejemplo:
-
-
- console.log(true && true); // true
- console.log(true && false); // false
- console.log(false && true); // false
- console.log(false && false); // false
+ /**El operador || (OR) devuelve true si al menos uno de los operandos es verdadero
+  *  y false solo si ambos operandos son falsos. Por ejemplo: */
+ 
+ console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
 
 
 
