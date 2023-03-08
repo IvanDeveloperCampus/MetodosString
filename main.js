65cb692e0fa248e24d9e3bf6cb3b4ade2b2c4a12
Ivan Sanchez <iivaan437@gmail.com>
2023-03-07 20:26:30 -0500

    feat: :sparkles: Metodo ".concat()"
    Implementacion del metodo  .concat  el cual une un texto con otro

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//charAt = retorna el eleento en la posicion dada
+//concat = retorna el elemento en la posicion dada
 let texto="Estoy aprendiendo todos los metodos de la clase String"
-
-console.log(texto.charAt(7)); //p
+let texto2="y tambien estoy aprendiendo a usar gitHub"
+console.log(texto.concat(texto2)); //Estoy aprendiendo todos los metodos de la clase String y tambien estoy aprendiendo a usar gitHub
