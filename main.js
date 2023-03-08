7a420d76aa1c9df1bda8e47e5a50fc22040fd555
Ivan Sanchez <iivaan437@gmail.com>
2023-03-07 20:56:58 -0500

    docs: :memo: Actualizar comentarios
    Le agregue una letra que faltaba en una palabra de los comentarios que estoy utilizando como documentacion

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-//split=convierte una cadena de texto en un array de plabras o caracteres
+//split=convierte una cadena de texto en un array de palabras o caracteres
 let texto=" Estoy aprendiendo todos los metodos de la clase String"
 console.log(texto.split(" ")); //['', 'Estoy', 'aprendiendo', 'todos', 'los', 'metodos', 'de', 'la', 'clase', 'String']
