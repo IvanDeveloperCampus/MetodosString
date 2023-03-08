43bdbaa1e0291487f2c7c8817125b07a096da1f6
Ivan Sanchez <iivaan437@gmail.com>
2023-03-07 20:54:56 -0500

    feat: :sparkles: Metodo .split()
    Implementacion del metodo ".split()" de la clase String

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-//slice= extrae una parte especifica del texto
+//split=convierte una cadena de texto en un array de plabras o caracteres
 let texto=" Estoy aprendiendo todos los metodos de la clase String"
-console.log(texto.slice(6,18)); //aprendiendo
+console.log(texto.split(" ")); //['', 'Estoy', 'aprendiendo', 'todos', 'los', 'metodos', 'de', 'la', 'clase', 'String']
