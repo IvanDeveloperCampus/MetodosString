a9f6e0c09f3c924770c5a92e79fac32295fb7975
Ivan Sanchez <iivaan437@gmail.com>
2023-03-08 17:00:12 -0500

    feat: :sparkles: Desigualdad Estricta
    Implementacion del operador desigualda estrictamente funcion inversamente a la igualdad estricta

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 
 //Operadores de Comparancion
 
-//Estrictamente (===) : Compara si son iguales y de el mismo tipó de dato
+// Desigualdad Estricta (!==) : Compara si son diferentes ya sea por valor o tipo de dato
 
 const num=254
 const num2="254"
 
-console.log(num!=num2);//False, son iguales pero diferente tipo de dat
+console.log(num!==num2);//True, son iguales pero diferente tipo de dat
 
 
 const num3="Hola"
 const num4="Hola"
-console.log(num3===num4); //True son iguales y de el mismo tipó de dato
+console.log(num3!==num4); //false son iguales y de el mismo tipó de dato
