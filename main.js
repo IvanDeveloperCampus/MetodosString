207ee2d670329440a20a857c155109ba7b256a9e
Ivan Sanchez <iivaan437@gmail.com>
2023-03-07 20:37:56 -0500

    refactor: :recycle:
    Agregue una nueva variable en la cual almacene el resultado de concatenar los textos

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //concat = retorna el elemento en la posicion dada
 let texto="Estoy aprendiendo todos los metodos de la clase String"
 let texto2="y tambien estoy aprendiendo a usar gitHub"
-console.log(texto.concat(texto2)); //Estoy aprendiendo todos los metodos de la clase String y tambien estoy aprendiendo a usar gitHub
+let texto3=texto.concat(texto2)
+console.log(texto3); //Estoy aprendiendo todos los metodos de la clase String y tambien estoy aprendiendo a usar gitHub
