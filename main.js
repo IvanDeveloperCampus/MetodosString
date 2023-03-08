7d5b8e842fa5ecd81126aa9738646f345f45aacb
Ivan Sanchez <iivaan437@gmail.com>
2023-03-07 20:42:39 -0500

    feat: :sparkles: Metodo .indexOf()
    implementacion del metodo  "indexOf()" de la clase String

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-//concat = retorna el elemento en la posicion dada
+//indexOf = retorna la posicion del elemento dado
 let texto="Estoy aprendiendo todos los metodos de la clase String"
-let texto2="y tambien estoy aprendiendo a usar gitHub"
-let texto3=texto.concat(texto2)
-console.log(texto3); //Estoy aprendiendo todos los metodos de la clase String y tambien estoy aprendiendo a usar gitHub
+console.log(texto.indexOf("p")); //7
