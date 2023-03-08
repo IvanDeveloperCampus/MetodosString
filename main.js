087e224e919e8b2987e05bbb7b40cd1535f53d52
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-03-08 06:15:09 -0500

    feat: :sparkles: Metodo .toLowerCase()
    Implementacion del metodo ".toLowerCase()" de la clase String

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 //trim=elimina los espacios al inicio o final de la cadena de texto
-let texto=" Estoy aprendiendo todos los metodos de la clase String "
-console.log(texto); // Estoy aprendiendo todos los metodos de la clase String
-console.log(texto.trim()); //Estoy aprendiendo todos los metodos de la clase String
+let texto="Estoy APRENDIENDO todos los metodos de la clase String"
+console.log(texto.toLowerCase()); // estoy aprendiendo todos los metodos de la clase string
