9829e5353c940b1f1b2a6780e87b151b01a5a5c7
Ivan Sanchez <iivaan437@gmail.com>
2023-03-08 17:02:51 -0500

    feat: :sparkles: Operador de comparacion (>) y (>=)
    Implementacion de los operadores de comparancion mayor que y mayor o igual que >, >=

--- main.js
+++ main.js
@@ -2,14 +2,10 @@
 
 //Operadores de Comparancion
 
-// Desigualdad Estricta (!==) : Compara si son diferentes ya sea por valor o tipo de dato
+// Mayor que (>) y (>=)
 
 const num=254
-const num2="254"
+const num2=254
 
-console.log(num!==num2);//True, son iguales pero diferente tipo de dat
-
-
-const num3="Hola"
-const num4="Hola"
-console.log(num3!==num4); //false son iguales y de el mismo tipó de dato
+console.log(num>num2);//False, el primer numero no es mayor 
+console.log(num>=num2); //el primero no es mayor pero si es igual
