26c01181a14210db99a27d4861e402f1538b3375
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-03-09 06:47:59 -0500

    feat: :sparkles: Math PI
    Uso del metodo Math PI

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 
 
-//Operadores Condicional
+//Objeto Math
 
-//  Ternario
+//PI
 
-edad=17
-
-const estado = (edad>=18) ? "Es mayor de edad" : "Es menor de edad";
-
-console.log(estado); //Es menor de edad
+const resultadoPi=Math.PI;
