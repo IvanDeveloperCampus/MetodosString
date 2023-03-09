77dfa8c5dcdb74e29c7df91fb33b3faf62ec2357
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-03-09 06:51:20 -0500

    feat: :sparkles: Math.round()
    El metodo .round del objeto Math redondea el numero al entero mas cerca, es decir si su decimal es mayor o igual que 5 redondea al siguiente numero si es menor que 5 redondea al mismo

--- main.js
+++ main.js
@@ -2,6 +2,14 @@
 
 //Objeto Math
 
-//PI
+//redondeo (round) se redonde al entero mas cercano
 
-const resultadoPi=Math.PI;
+const resultadoRedondeo=Math.round(2.4); 
+
+console.log(resultadoRedondeo);//2
+
+
+const resultadoRedondeo2=Math.round(2.7); 
+
+console.log(resultadoRedondeo2);//3
+
