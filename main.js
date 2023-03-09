45711a9bdbf788af137ac6bf7036a1253a83875b
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-03-09 07:12:32 -0500

    feat: :sparkles: Math.floor() and Math.ceil()
    El primer metodo .floor() redondea el numero al anterior y el otro .ceil() redondea el numero al siguiente

--- main.js
+++ main.js
@@ -2,17 +2,10 @@
 
 //Objeto Math
 
-//Random =  nos escoge un numero al azar
+// Round superior e inferior
 
-const resultadoRandom=Math.random()
+const resultaInferior=Math.floor(2.8)
+console.log(resultaInferior);//2
 
-console.log(resultadoRandom); //Numero X
-
-
-//Escoje un numero entre 0 y 10 
-console.log(Math.floor(Math.random() * 10));
-
-
-//Escoje un numero entre 200 y 215
-console.log(Math.floor(Math.random()* 15) + 200);
-
+const resultaSuperior=Math.ceil(3.25)
+console.log(resultaSuperior);//4
